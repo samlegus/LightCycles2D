b0ecc8bf681fb9fbf0cc2ce3a5b3cef67feb74ad
unknown <towhite@WWSTPSRM21201.seattleschools.org>
2015-05-11 16:50:27 -0700

    Level loading prefab + example switch scene on button press

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour
+{
+	public string MainGameSceneName = "Game";
+	public string MainMenuSceneName = "MainMenu";
+	public string GameOverSceneName = "GameOver";
+
+	public void LoadMainGame()
+	{
+		Application.LoadLevel(MainGameSceneName);
+	}
+
+	public void LoadMainMenu()
+	{
+		Application.LoadLevel (MainMenuSceneName);
+	}
+
+	public void LoadGameOver()
+	{
+		Application.LoadLevel (GameOverSceneName);
+	}
+
+}
